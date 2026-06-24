2fdc9578062ee1500d918d47105a69c7413b8805
DethPlague98 <tnaes98@gmail.com>
2026-06-23 20:11:44 -0500

    initial project

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+const path = require("path");
+const fileHandler = require('./fileHandler');
+const taskManager = require('./taskManager');
 
+//Define the file path for tasks.json
+const filePath = path.join(__dirname,"tasks.json");
+
+//Load existing tasks
+let tasks = fileHandler.loadTasks(filePath);
+//Add a new task
+taskManager.addTask(tasks, "wash dishes");
+//List all tasks
+taskManager.listTask(tasks);
+//Save new updated task list
+fileHandler.saveTasks(filePath, tasks);
--- taskManager.js
+++ taskManager.js
@@ -1,1 +1,14 @@
 
+
+function addTask(tasks, task){
+    tasks.push(task);
+    console.log(`Task "${task}" added!`)
+}
+
+function listTask(tasks){
+    console.log(`Here is the current list of tasks:
+    Tasks:
+    ${tasks}`);
+}
+
+module.exports = {addTask, listTask};
